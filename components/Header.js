70f402b997aade49e48e59c0bbbe530c54dd6377
Oliver van den Bosch <vanders06@icloud.com>
2021-05-15 22:11:59 +0100

    Update styling, fonts

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,6 @@ import locationIcon from "@iconify/icons-mdi/fire-alert";
 
 export default function Header() {
   return <header className="header">
-      <h1><Icon icon={locationIcon}/> Wildfire Tracker (Powered By NASA)</h1>
+      <h1><Icon icon={locationIcon}/> Wildfire Tracker (Powered By <a href="https://api.nasa.gov/">NASA</a>)</h1>
   </header>;
 }
